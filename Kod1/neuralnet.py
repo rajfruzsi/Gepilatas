3880c598a5222af2f5cff2a66b8f5c367663ed27
rajfruzsi <34722134+rajfruzsi@users.noreply.github.com>
2019-05-31 15:07:05 +0200

    Update neuralnet.py
    tesztresz kikommentezve

--- Kod1/neuralnet.py
+++ Kod1/neuralnet.py
@@ -85,9 +85,12 @@ plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
 model.save('neuralnet.h5')
+#tesztrész
+'''
 image_index = 334
 plt.imshow(X_test[image_index].reshape(28, 28))
 pred = model.predict(X_test[image_index].reshape(1, 784))
 print(pred.argmax())
 plt.show()
+'''
 
